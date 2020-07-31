f48b94b2c23c5efeeea1da3b99e2fac6151499c6
emuhlestein <emuhlestein@yahoo.com>
2020-07-31 16:00:45 -0500

    Fix unit tests

--- src/test/java/com/intelliviz/familymanage/repository/FoodProductTypeRepositoryTest.java
+++ src/test/java/com/intelliviz/familymanage/repository/FoodProductTypeRepositoryTest.java
@@ -3,6 +3,8 @@ package com.intelliviz.familymanage.repository;
 import com.intelliviz.familymanage.model.FoodProduct;
 import com.intelliviz.familymanage.model.FoodProductType;
 import org.hibernate.exception.ConstraintViolationException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +14,39 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FoodProductTypeRepositoryTest {
 
-	private static final String TEST_TYPE = "TEST_TYPE";
+	private static final String TEST_TYPE1 = "TEST_TYPE1";
+	private static final String TEST_TYPE2 = "TEST_TYPE2";
+
 	@Autowired
 	FoodProductTypeRepository repo;
 
 	@Autowired
 	EntityManager em;
 
+	@Before
+	public void setup() {
+		deleteItem(TEST_TYPE1);
+		FoodProductType fpt1 = repo.save(new FoodProductType(TEST_TYPE1));
+	}
+
+	@After
+	public void cleanup() {
+		deleteItem(TEST_TYPE1);
+	}
+
 	@Test
 	@DirtiesContext
 	public void testGetById() {
-		FoodProductType fpt1 = repo.save(new FoodProductType(TEST_TYPE));
+		FoodProductType fpt1 = repo.findByName(TEST_TYPE1);
 		FoodProductType fpt2 = repo.findById(fpt1.getId());
 		assertEquals(fpt1.getId(), fpt2.getId());
 	}
@@ -36,25 +54,23 @@ public class FoodProductTypeRepositoryTest {
 	@Test
 	@DirtiesContext
 	public void testGetByName() {
-		FoodProductType fpt1 = repo.save(new FoodProductType(TEST_TYPE));
-		FoodProductType fpt2 = repo.findByName(TEST_TYPE);
-		assertEquals(fpt1.getName(), fpt2.getName());
+		FoodProductType fpt = repo.findByName(TEST_TYPE1);
+		assertEquals(TEST_TYPE1, fpt.getName());
 	}
 
 	@Test
 	@DirtiesContext
 	public void testAddProductType() {
-		FoodProductType fpt1 = repo.save(new FoodProductType(TEST_TYPE));
+		FoodProductType fpt1 = repo.save(new FoodProductType(TEST_TYPE2));
 		FoodProductType fpt2 = repo.findById(fpt1.getId());
 		assertEquals(fpt1.getId(), fpt2.getId());
+		repo.deleteById(fpt1.getId());
 	}
 
 	@Test
 	@DirtiesContext
 	public void testAddDuplicateProductType() {
-		FoodProductType fpt = new FoodProductType(TEST_TYPE);
-		repo.save(fpt);
-		fpt = new FoodProductType(TEST_TYPE);
+		FoodProductType fpt = new FoodProductType(TEST_TYPE1);
 		try {
 			repo.save(fpt);
 		} catch(DataIntegrityViolationException e) {
@@ -70,4 +86,15 @@ public class FoodProductTypeRepositoryTest {
 			assertTrue(t instanceof ConstraintViolationException);
 		}
 	}
+
+	private void deleteItem(String itemToDelete) {
+		List<FoodProductType> list = repo.getAll();
+		List<FoodProductType> fptList = list.stream()
+				.filter(item -> item.getName().equals(itemToDelete))
+				.collect(Collectors.toList());
+		if(fptList.size() > 0) {
+			repo.deleteById(fptList.get(0).getId());
+		}
+	}
+
 }
