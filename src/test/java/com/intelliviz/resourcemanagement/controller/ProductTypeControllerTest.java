f9036009bd155e0923c2d3772fc539074637bbad
emuhlestein <emuhlestein@yahoo.com>
2020-10-20 15:10:39 -0500

    Add unit test for producttype post method.

--- src/test/java/com/intelliviz/resourcemanagement/controller/ProductTypeControllerTest.java
+++ src/test/java/com/intelliviz/resourcemanagement/controller/ProductTypeControllerTest.java
@@ -1,13 +1,20 @@
 package com.intelliviz.resourcemanagement.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.intelliviz.resourcemanagement.model.ProductType;
 import com.intelliviz.resourcemanagement.service.ProductTypeServiceImpl;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -16,9 +23,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @WebMvcTest(ProductTypeController.class)
 public class ProductTypeControllerTest {
@@ -93,5 +104,35 @@ public class ProductTypeControllerTest {
         mockMvc.perform(request)
                 .andExpect(status().isNotFound()) // test for 404
                 .andReturn();
+    }
+
+    @Test
+    void postProductTypeTest() throws Exception {
+        ProductType mockProductType = new ProductType(1, "Test", "test");
+        when(productTypeService.save(Mockito.any(ProductType.class))).thenReturn(mockProductType);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = mapper.writeValueAsString(mockProductType);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/producttype")
+                .accept(MediaType.APPLICATION_JSON).content(jsonInString)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(jsonPath("$.id", Matchers.equalTo(1)))
+                .andReturn();
+        MockHttpServletResponse response = result.getResponse();
+        String contentString = response.getContentAsString();
+        assertNotNull(contentString);
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(contentString, jsonInString);
+
+        ProductType productType = mapper.readValue(response.getContentAsString(), ProductType.class);
+        assertEquals(mockProductType.getId(), productType.getId());
+        assertEquals(mockProductType.getName(), productType.getName());
+        assertEquals(mockProductType.getDescription(), productType.getDescription());
+
+        System.out.println(response.getHeader(HttpHeaders.LOCATION));
     }
 }
