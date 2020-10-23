3c33f4a848426528ea5e0f1f85350e2dad9c9839
emuhlestein <emuhlestein@yahoo.com>
2020-10-23 17:04:00 -0500

    Update product table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,22 +7,23 @@ CREATE TABLE IF NOT EXISTS product_type (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS container_type (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 --DROP TABLE IF EXISTS product;
 
 CREATE TABLE IF NOT EXISTS product (
     id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(64) NOT NULL,
     description VARCHAR(256) NOT NULL,
-    container INTEGER NOT NULL,
+    container_type_id INTEGER NOT NULL,
     weight_per_container FLOAT NOT NULL,
     product_type_id INTEGER NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (product_type_id) REFERENCES product_type(id)
-);
-
-CREATE TABLE IF NOT EXISTS container_type (
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    name VARCHAR(64) NOT NULL,
-    description VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    FOREIGN KEY (container_type_id) REFERENCES container_type(id)
 );
